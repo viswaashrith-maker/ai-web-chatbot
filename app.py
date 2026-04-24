4aeadc672f15ba3048d42ab8c829f22139ae23a6
Ashrith <viswaashrith@gmail.com>
2026-04-25 00:47:36 +0530

    fix render port

--- app.py
+++ app.py
@@ -53,11 +53,7 @@ def clear_chat():
     global chat_history
     chat_history = []
     return redirect("/")
-
-
+import os
 if __name__ == "__main__":
-    import os
-
-    if __name__ == "__main__":
-        port = int(os.environ.get("PORT", 5000))
-        app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
